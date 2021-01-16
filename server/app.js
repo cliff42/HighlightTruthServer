2f7544ae4485d5f471a8e70f5ae848585914a64d
Christopher Cliff <45251852+cliff42@users.noreply.github.com>
2021-01-16 01:21:56 -0800

    fixes w/ try catch

--- server/app.js
+++ server/app.js
@@ -27,42 +27,44 @@ app.post('/postText', async (req, res) => {
 
     console.log(q, start, num);
 
-    customsearch.cse.list({
-        auth: config.GCP_API_KEY,
-        cx: config.GCP_CX,
-        q, start, num
-    })
-
-    .then(result => result.data)
-    .then((result) => {
-    const { queries, items, searchInformation } = result;
-
-    const page = (queries.request || [])[0] || {};
-    const previousPage = (queries.previousPage || [])[0] || {};
-    const nextPage = (queries.nextPage || [])[0] || {};
-
-    const data = {
-        q,
-        totalResults: page.totalResults,
-        count: page.count,
-        startIndex: page.startIndex,
-        nextPage: nextPage.startIndex,
-        previousPage: previousPage.startIndex,
-        time: searchInformation.searchTime,
-        items: items.map(o => ({
-        link: o.link,
-        title: o.title,
-        snippet: o.snippet,
-        img: (((o.pagemap || {}).cse_image || {})[0] || {}).src
-        }))
+    try {
+        customsearch.cse.list({
+            auth: config.GCP_API_KEY,
+            cx: config.GCP_CX,
+            q, start, num
+        })
+    
+        .then(result => result.data)
+        .then((result) => {
+        const { queries, items, searchInformation } = result;
+    
+        const page = (queries.request || [])[0] || {};
+        const previousPage = (queries.previousPage || [])[0] || {};
+        const nextPage = (queries.nextPage || [])[0] || {};
+    
+        const data = {
+            q,
+            totalResults: page.totalResults,
+            count: page.count,
+            startIndex: page.startIndex,
+            nextPage: nextPage.startIndex,
+            previousPage: previousPage.startIndex,
+            time: searchInformation.searchTime,
+            items: items.map(o => ({
+            link: o.link,
+            title: o.title,
+            snippet: o.snippet,
+            img: (((o.pagemap || {}).cse_image || {})[0] || {}).src
+            }))
+        }
+        // res.status(200).send(result);
+        res.status(200).send(data);
+        })
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
     }
-    // res.status(200).send(result);
-    res.status(200).send(data);
-    })
-    .catch((err) => {
-    console.log(err);
-    res.status(500).send(err);
-    });
 
 });
 
